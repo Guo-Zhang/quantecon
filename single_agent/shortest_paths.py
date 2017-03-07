d57be810ccc89de7693ad8227bc5ad02878c41df
Guo ZHANG <iguo19961110@foxmail.com>
2017-03-07 23:52:38 +0800

    Create

---
+++ single_agent/shortest_paths.py
@@ -0,0 +1,57 @@
+# -*-coding:utf-8-*-
+
+
+"""
+Author: Guo Zhang
+Create Date: 2017-03-02
+Description: Shortest Paths, Simple Agent Models
+"""
+
+
+class Graph(object):
+    """
+    pass
+    """
+    def __init__(self):
+        pass
+
+
+class JFunction(object):
+    """
+    pass
+    """
+
+    def __init__(self):
+        pass
+
+    def __equal__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+
+def read_data():
+    pass
+
+
+def f():
+    pass
+
+
+def shortest_paths():
+    n = 0
+    while j_instance != j_instance:
+        j_instance = f(j_instance)
+    return j_instance
+
+
+def main():
+    pass
+
+
+if __name__=='__main__':
+    main()
